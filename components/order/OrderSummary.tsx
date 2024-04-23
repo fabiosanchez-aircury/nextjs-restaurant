65f899f39361e37b8546406dee1fbfa5cd380554
fabiosanchez-aircury <fabio.sanchez@aircury.com>
2024-04-23 16:24:05 +0200

    global state created

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,7 +1,20 @@
+"use client";
+import { useStore } from "@/src/store";
+
 export default function OrderSummary() {
+  const order = useStore((state) => state.order);
+
   return (
     <aside className="lg:h-screen lg:overflow-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi pedido</h1>
+
+      {order.length === 0 ? (
+        <p className="text-center my-10">El carrito esta vacío</p>
+      ) : (
+        <div className="mt-5">
+          <p>cosas</p>
+        </div>
+      )}
     </aside>
   );
 }
